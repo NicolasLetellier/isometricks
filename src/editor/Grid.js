3d8187678d60a2a6047bf769bd8a3bb91a77b9c9
Nicolas Letellier <mrletelliernicolas@yahoo.fr>
2019-10-08 00:11:49 +0200

    add first experiment with svg polygon

--- src/editor/Grid.js
+++ src/editor/Grid.js
@@ -4,9 +4,14 @@ import './Grid.css';
 
 function Grid({ map, activeFace }) {
   return (
-    <div className="Grid">
-
-    </div>
+    <svg className="Grid" viewBox="0 0 5 5">
+      <polygon
+        points="1.2,2 3,4 4,4"
+        stroke="green"
+        fill="transparent"
+        stroke-width="0.1"
+      />
+    </svg>
   );
 }
 
