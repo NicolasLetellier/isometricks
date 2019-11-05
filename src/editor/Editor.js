1b2b65f9c0a3a2d2b63237bd0914ebb7fa4d1d00
Nicolas Letellier <mrletelliernicolas@yahoo.fr>
2019-11-05 12:35:37 +0100

    add backward and forward history navigation

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -21,8 +21,59 @@ function Editor() {
   // history array of polygon stacks
   // polygon stack: array of polygon objects to be draw
   const [stacksHistory, setStacksHistory] = useState([[]]);
+  // historyNavIndex === null : not currently in history navigation
+  const [historyNavIndex, setHistoryNavIndex] = useState(null);
+
+  // length shouldn't be less than 1
+  const historyLastIndex = stacksHistory.length - 1;
 
   const trianglesMap = trianglesMapBuilder(gridDimensionsInTriangles.width, gridDimensionsInTriangles.height);
+
+  function backwardInHistory() {
+    if (historyNavIndex === null) {
+      setHistoryNavIndex(historyLastIndex - 1);
+    } else {
+      setHistoryNavIndex(historyNavIndex - 1);
+    }
+  }
+
+  function backwardInHistoryProp() {
+    if (historyLastIndex === 0 || historyNavIndex === 0) {
+      return null;
+    } else {
+      return backwardInHistory;
+    }
+  }
+
+  function forwardInHistory() {
+    if (historyNavIndex === historyLastIndex - 1) {
+      setHistoryNavIndex(null);
+    } else {
+      setHistoryNavIndex(historyNavIndex + 1);
+    }
+  }
+
+  function forwardInHistoryProp() {
+    if (historyNavIndex === null) {
+      return null;
+    } else {
+      return forwardInHistory;
+    }
+  }
+
+  function currentHistoryIndex() {
+    return (historyNavIndex === null) ? historyLastIndex : historyNavIndex;
+  }
+
+  // truncate forward history to add new stack if currently in history navigation
+  function addPolygonToStack(polygon) {
+    const previousPolygonStack = stacksHistory[currentHistoryIndex()];
+    const actualisedPolygonStack = previousPolygonStack.concat(polygon);
+    const slicedHistory = stacksHistory.slice(0, currentHistoryIndex() + 1);
+    slicedHistory.push(actualisedPolygonStack);
+    setStacksHistory(slicedHistory);
+    setHistoryNavIndex(null);
+  }
 
   function triangleClickHandler(triangleMapCoord, triangleMapData) {
     if (activeFace === null) { // activeFace set to null: inactivate any click event
@@ -58,21 +109,21 @@ function Editor() {
       // similar faces color at once and for detecting exactly repeated faces
     };
 
-    const previousPolygonStack = stacksHistory[stacksHistory.length - 1];
-    const actualisedPolygonStack = previousPolygonStack.concat(polygon);
-    const stacksHistoryCopy = [...stacksHistory];
-    stacksHistoryCopy.push(actualisedPolygonStack);
-    setStacksHistory(stacksHistoryCopy);
+    addPolygonToStack(polygon);
   }
 
   return (
     <div className="Editor">
-      <Toolbar setActiveFace={setActiveFace} />
+      <Toolbar
+        setActiveFace={setActiveFace}
+        backwardInHistory={backwardInHistoryProp()}
+        forwardInHistory={forwardInHistoryProp()}
+      />
       <Frame
         gridDimensionsInTriangles={gridDimensionsInTriangles}
         trianglesMap={trianglesMap}
         triangleClickHandler={triangleClickHandler}
-        polygonStack={stacksHistory[stacksHistory.length - 1]}
+        polygonStack={stacksHistory[currentHistoryIndex()]}
       />
     </div>
   );
