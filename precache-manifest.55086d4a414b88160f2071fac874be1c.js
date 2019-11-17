3b9b6b5e9adbb30a99df41e691d5585e8c9d87e8
NicolasLetellier <mrletelliernicolas@yahoo.fr>
2019-11-17 22:26:45 +0100

    Updates

---
+++ precache-manifest.55086d4a414b88160f2071fac874be1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e130c3ff122450b899393c5e6b2eb9dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a2198078d25d0c657a1",
+    "url": "/static/css/main.5be1de81.chunk.css"
+  },
+  {
+    "revision": "a629bcbbe1b735492faa",
+    "url": "/static/js/2.5ec3120b.chunk.js"
+  },
+  {
+    "revision": "2a2198078d25d0c657a1",
+    "url": "/static/js/main.62ff3221.chunk.js"
+  },
+  {
+    "revision": "a5397c7886a48e7442b4",
+    "url": "/static/js/runtime-main.11238b65.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
